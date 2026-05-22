f13581d909a99d7342d7a5775cbf994b3d7bd334
Artalus <artalus-mail@yandex.ru>
2026-05-23 01:51:57 +0400

    disable ai in tests

--- PetMapMarkers.gametests/AutotestModSystem.cs
+++ PetMapMarkers.gametests/AutotestModSystem.cs
@@ -14,4 +14,14 @@ public class AutotestModSystem : GametestModsystemBase
         int stepMs = SApi.ModLoader.GetModSystem<PetMapMarkersModSystem>().Tracker.IntervalMs * 2;
         return [new PetMarkerTestCases(SApi, player, stepMs, ChunkLoadMs)];
     }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+
+        // disable AI tasks and activity system for the test world so freshly spawned
+        // NPCs remain idle during tests
+        api.World.Config.SetBool("runAiTasks", false);
+        api.World.Config.SetBool("runAiActivities", false);
+    }
 }
